5e41418ff0a7d1e221539df176d5cf9a6e71469c
iman memarpour <iman.memarpour@gmail.com>
2021-03-31 04:06:39 -0400

    question8

--- VogCodeChallenge.Console/QuestionClass.cs
+++ VogCodeChallenge.Console/QuestionClass.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace VogCodeChallenge.Console
 {
@@ -10,5 +11,52 @@ namespace VogCodeChallenge.Console
             "Jeffrey",
             "John",
         };
+
+        public static T TESTModule<T>(T input)
+        {
+            var caseValue = DetermineCase(input);
+
+            switch (caseValue)
+            {
+                case "A":
+                    return (T)Convert.ChangeType(Convert.ToInt32(input) * 2, typeof(T));
+                case "B":
+                    return (T)Convert.ChangeType(Convert.ToInt32(input) * 3, typeof(T));
+                case "C":
+                    throw new ArgumentOutOfRangeException();
+                case "D":
+                    return (T)Convert.ChangeType(3.0f, typeof(T));
+                case "E":
+                    return (T)Convert.ChangeType(input.ToString()?.ToUpper(), typeof(T));
+                case "F":
+                    return input;
+                default:
+                    return input;
+            }
+        }
+
+        private static string DetermineCase<T>(T input)
+        {
+            if (typeof(T) == typeof(int))
+            {
+                if (Convert.ToInt32(input) >= 1 && Convert.ToInt32(input) <= 4) return "A";
+                if (Convert.ToInt32(input) <= 0) return "C";
+                return "B";
+            }
+
+            if (typeof(T) == typeof(float))
+            {
+                if (Math.Abs((float)(object)input - 1.0f) < 0.001 || Math.Abs((float)(object)input - 2.0f) < 0.001)
+                    return "D";
+            }
+
+            if (typeof(T) == typeof(string))
+            {
+                if (!string.IsNullOrWhiteSpace(input.ToString()))
+                    return "E";
+            }
+
+            return "F";
+        }
     }
 }
