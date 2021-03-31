c305bb3ff26fedd989b87eb762d9260cfa6d6b5b
iman memarpour <iman.memarpour@gmail.com>
2021-03-31 04:02:34 -0400

    question7

---
+++ VogCodeChallenge.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace VogCodeChallenge.Console
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            using var enumerator = QuestionClass.NamesList.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                System.Console.WriteLine(enumerator.Current);
+            }
+        }
+    }
+}
---
+++ VogCodeChallenge.Console/QuestionClass.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace VogCodeChallenge.Console
+{
+    public class QuestionClass
+    {
+        public static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "Jeffrey",
+            "John",
+        };
+    }
+}
